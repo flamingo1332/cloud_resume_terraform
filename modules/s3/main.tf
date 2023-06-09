a04bc2e4cd06916fa6f55fc0c4d4c253ca6e5547
flamingo1332 <ksw2955@naver.com>
2023-06-09 17:49:57 +0900

    s3 fix

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -17,10 +17,10 @@ resource "aws_s3_bucket_ownership_controls" "frontend_bucket_controls" {
 resource "aws_s3_bucket_public_access_block" "frontend_bucket_control_access" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_versioning" "frontend_bucket_versioning" {
